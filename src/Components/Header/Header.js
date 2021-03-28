42dd51e15917b659ad6bf36f6a7fc912bb65a04d
Sanjay Sunil <sanjaysunil@protonmail.com>
2021-03-28 12:04:06 +0100

    Added GitHub icon in header.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,16 +10,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
+  },
+  button: {
+    textDecoration: 'none',
+    color: '#fff',
   },
 }));
 
@@ -33,6 +36,11 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             CovidTracker by Sanjay Sunil
           </Typography>
+          <a href="https://github.com/sanjaysunil/covidtracker" rel="noreferrer" target="_blank" className={classes.button}>
+            <IconButton aria-label="GitHub" color="inherit">
+              <GitHubIcon />
+            </IconButton>
+          </a>
         </Toolbar>
       </AppBar>
     </div>
