6025b59898f819884322173d002d15a12bf56dd6
Sanjay Sunil <sanjaysunil@protonmail.com>
2021-03-28 11:41:40 +0100

    Added better styling for lastUpdated.

--- src/Components/Tabs/Home/Stats/HomeStats.js
+++ src/Components/Tabs/Home/Stats/HomeStats.js
@@ -32,6 +32,18 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '3vh',
     fontWeight: 'bolder',
   },
+  lastUpdated: {
+    '&': {
+      fontSize: '20px',
+      color: 'grey',
+      textDecoration: 'none',
+      transition: '0.25s',
+    },
+    '&:hover': {
+      color: '#fff',
+      transition: '0.25s',
+    },
+  },
 }));
 
 const HomeStats = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
@@ -107,7 +119,7 @@ const HomeStats = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </Grid>
           <Grid item xs={12}>
             <Paper elevation={3} className={classes.paper}>
-              <span className={classes.count}>
+              <span className={classes.lastUpdated}>
                 Last updated on {new Date(lastUpdate).toDateString()}
               </span>
             </Paper>
